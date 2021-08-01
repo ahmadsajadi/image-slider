a2c1bb8c2375844925faf3399e3237e046721ff3
fanap_soft_26 <ahmadsajadidev73@gmail.com>
2021-08-01 11:36:51 +0430

    add Explayer

--- image-slider/src/main/java/com/example/image_slider/Slider.kt
+++ image-slider/src/main/java/com/example/image_slider/Slider.kt
@@ -1,18 +1,38 @@
 package com.example.image_slider
 
+import android.content.Context
+import com.google.android.exoplayer2.SimpleExoPlayer
+
+
+
+
 class Slider {
+    var player : SimpleExoPlayer? = null
+    var array = ArrayList<String>()
     var x = 10
 
     fun getx():Int{
         return x
     }
 
-    var array = ArrayList<String>()
+
     fun getString(): String {
         array.add("tester")
         array.add("hello1")
         array.add("hello2")
         array.add("hello3")
         return array.filter { s -> s.equals("tester") }.first()
+    }
+
+    fun setPlayer(context: Context){
+        player = SimpleExoPlayer.Builder(context).build()
+    }
+
+    fun play(){
+        player?.play()
+    }
+
+    fun stop(){
+        player?.stop()
     }
 }
