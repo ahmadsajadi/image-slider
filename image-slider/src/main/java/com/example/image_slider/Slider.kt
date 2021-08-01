fb8428aa66cc16782e973bf83bdda3dc76a8653b
fanap_soft_26 <ahmadsajadidev73@gmail.com>
2021-08-01 11:19:40 +0430

    add getString

--- image-slider/src/main/java/com/example/image_slider/Slider.kt
+++ image-slider/src/main/java/com/example/image_slider/Slider.kt
@@ -6,4 +6,13 @@ class Slider {
     fun getx():Int{
         return x
     }
+
+    var array = ArrayList<String>()
+    fun getString(): String {
+        array.add("tester")
+        array.add("hello1")
+        array.add("hello2")
+        array.add("hello3")
+        return array.filter { s -> s.equals("tester") }.first()
+    }
 }
